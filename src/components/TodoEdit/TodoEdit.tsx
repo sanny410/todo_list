9ff4d4760d33151ec88c1d0b8567f5286d3b2f56
Evgeniya <74735099+sanny410@users.noreply.github.com>
2022-08-02 16:18:25 +0300

    make a task edit page

--- src/components/TodoEdit/TodoEdit.tsx
+++ src/components/TodoEdit/TodoEdit.tsx
@@ -1,9 +1,21 @@
 import React from 'react';
+import './style.scss'
 
 const TodoEdit = () => {
     return (
         <div className="todo-list__edit-item">
-
+            <h2 className="edit-item__title">Редактирование задачи №1</h2>
+            <form>
+                <select className="task__status">
+                    <option className="status__item">Выполнена</option>
+                    <option className="status__item">В процессе</option>
+                    <option className="status__item">Ожидает</option>
+                </select>
+                <input className="task__text input" type="text" placeholder="Текст задачи"/>
+                <input className="task__text input" type="text" placeholder="Исполнитель"/>
+                <textarea className="task__comment input" rows={3} placeholder="Комментарий (не обязательно)"></textarea>
+                <button className="btn edit__task">Редактировать</button>
+            </form>
         </div>
     );
 };
