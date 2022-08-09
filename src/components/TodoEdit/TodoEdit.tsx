d3e14d5ac305b555587779be74c187f31d28fc87
Evgeniya <74735099+sanny410@users.noreply.github.com>
2022-08-09 12:51:38 +0300

    make color indication of tasks

--- src/components/TodoEdit/TodoEdit.tsx
+++ src/components/TodoEdit/TodoEdit.tsx
@@ -19,51 +19,59 @@ const TodoEdit = ({editTodoItem, setEditTodoItem, todo, setTodo}
 
     return (
         <div className="todo-list__edit-item">
-            <h2 className="edit-item__title">Редактирование задачи №{editTodoItem.id}</h2>
-            <form>
-                <select className="task__status" value={status} onChange={(event: React.ChangeEvent<HTMLSelectElement>) => setStatus(event.target.value)} >
-                    <option className="status__item">Выполнена</option>
-                    <option className="status__item">В процессе</option>
-                    <option className="status__item">Ожидает</option>
-                </select>
-                <input className="task__text input"
-                       value={task}
-                       type="text"
-                       placeholder="Текст задачи"
-                       onChange={(event: React.ChangeEvent<HTMLInputElement>) => setTask(event.target.value)}/>
-                <input className="task__text input"
-                       type="text"
-                       placeholder="Исполнитель"
-                       value={executor}
-                       onChange={(event: React.ChangeEvent<HTMLInputElement>) => setExecutor(event.target.value)}/>
-                <textarea className="task__comment input"
-                          rows={3}
-                          placeholder="Комментарий (не обязательно)"
-                          value={comment}
-                          onChange={(event: React.ChangeEvent<HTMLTextAreaElement>) => setComment(event.target.value)}/>
-                <button className="btn edit__task"
-                onClick={(event) => {
-                    event.preventDefault()
-                    let indexItem = todo.indexOf(editTodoItem);
-                    let newArr = todo.slice();
-                    newArr[indexItem] = {
-                        id: editTodoItem.id,
-                        task: task,
-                        status: status,
-                        executor: executor,
-                        comment: comment
-                    }
-                    setTodo(newArr);
-                    setEditTodoItem({
-                        id: 0,
-                        task: "",
-                        status: "",
-                        executor: "",
-                        comment: ""
-                    })
-                    }}>
-                    Редактировать</button>
-            </form>
+            {(editTodoItem.id === 0) ?
+                    <h2 className="edit-item__title">Для редактирования задачи нажмите  <img src={require('../../assets/edit.png')} alt="edit_task"/></h2>
+                    :
+                <>
+                    <h2 className="edit-item__title">Редактирование задачи №{editTodoItem.id}</h2>
+                    <form>
+                        <select className="task__status" value={status}
+                                onChange={(event: React.ChangeEvent<HTMLSelectElement>) => setStatus(event.target.value)}>
+                            <option className="status__item">Выполнена</option>
+                            <option className="status__item">В процессе</option>
+                            <option className="status__item">Ожидает</option>
+                        </select>
+                        <input className="task__text input"
+                               value={task}
+                               type="text"
+                               placeholder="Текст задачи"
+                               onChange={(event: React.ChangeEvent<HTMLInputElement>) => setTask(event.target.value)}/>
+                        <input className="task__text input"
+                               type="text"
+                               placeholder="Исполнитель"
+                               value={executor}
+                               onChange={(event: React.ChangeEvent<HTMLInputElement>) => setExecutor(event.target.value)}/>
+                        <textarea className="task__comment input"
+                                  rows={3}
+                                  placeholder="Комментарий (не обязательно)"
+                                  value={comment}
+                                  onChange={(event: React.ChangeEvent<HTMLTextAreaElement>) => setComment(event.target.value)}/>
+                        <button className="btn edit__task"
+                                onClick={(event) => {
+                                    event.preventDefault();
+                                    let indexItem = todo.indexOf(editTodoItem);
+                                    let newArr = todo.slice();
+                                    newArr[indexItem] = {
+                                        id: editTodoItem.id,
+                                        task: task,
+                                        status: status,
+                                        executor: executor,
+                                        comment: comment
+                                    };
+                                    setTodo(newArr);
+                                    setEditTodoItem({
+                                        id: 0,
+                                        task: "",
+                                        status: "",
+                                        executor: "",
+                                        comment: ""
+                                    });
+                                }}>
+                            Редактировать
+                        </button>
+                    </form>
+                </>
+            }
         </div>
     );
 };
--- src/components/TodoList/TodoItem.tsx
+++ src/components/TodoList/TodoItem.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import './style.scss'
 
 export type Task = {
@@ -9,10 +9,19 @@ export type Task = {
     comment: string
 }
 
-const TodoItem = ({item, id, setTodo, todo, setEditTodoItem, editTodoItem }:
+const TodoItem = ({item, id, setTodo, todo, setEditTodoItem}:
                       {item: Task, id: number, setTodo: Function, todo: Task[],
                           setEditTodoItem: Function, editTodoItem: Task}) => {
 
+    const [colorStatus, setColorStatus] = useState('expect');
+
+    useEffect(() => {
+        if(item.status === "Ожидает") {
+            setColorStatus('expect')
+        } else if(item.status === "В процессе") {
+            setColorStatus('in_the_process')
+        } else setColorStatus('finished')
+    }, [item.status])
 
     const deleteTask = (id: number) => {
         setTodo([...todo.filter((card: Task) => card.id !== item.id)])
@@ -28,7 +37,7 @@ const TodoItem = ({item, id, setTodo, todo, setEditTodoItem, editTodoItem }:
 
 
     return (
-        <li className="todo-list__item">
+        <li className={`todo-list__item ${colorStatus}`}>
             <div>{id}. {item.task} </div>
             <div className="icons">
                 <img src={require('../../assets/delete.png')} alt="delete_task" onClick={() => deleteTask(id)}/>
